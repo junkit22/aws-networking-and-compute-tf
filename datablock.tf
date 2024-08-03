0811699504a8baa3459b57a83519fc7ef4ecd711
junkit22 <junkit22@gmail.com>
2024-08-03 15:15:29 +0800

    will always call for latest ami

--- datablock.tf
+++ datablock.tf
@@ -1,21 +1,21 @@
 data "aws_ami" "latest_ami" {
-  executable_users = ["self"]
-  most_recent      = true
-  name_regex       = "^myami-\\d{3}"
-  owners           = ["self"]
+  most_recent = true
 
   filter {
     name   = "name"
-    values = ["myami-*"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]  # Replace with your desired AMI name pattern
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
+  owners = ["amazon"]  # Replace with the owner ID if you want to restrict to a specific owner
+}
+
+
+output "latest_ami_id" {
+  value = data.aws_ami.latest_ami.id
+  description = "The ID of the latest AMI"
 }
